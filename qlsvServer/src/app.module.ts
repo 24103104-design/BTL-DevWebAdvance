35d2bc64d100388ba15075d09af452f3482225c3
hellotoilatruongvandat-rgb <hellotoilatruongvandat@gmail.com>
2026-06-18 08:57:04 +0700

    Setup TypeORM with MySQL and include BookModule
    Configured TypeORM with MySQL connection settings and added BookModule.

--- qlsvServer/src/app.module.ts
+++ qlsvServer/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BookModule } from './book/book.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'điền-host-aiven',
+      port: 'điền-port-aiven' as unknown as number,
+      username: 'điền-username',
+      password: 'điền-password',
+      database: 'điền-tên-database',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      ssl: { rejectUnauthorized: false },
+    }),
+    BookModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
