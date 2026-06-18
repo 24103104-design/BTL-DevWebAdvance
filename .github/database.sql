a54c60b7d11d619418d906545c887284b1abcc34
24103104-design <24103104@st.phenikaa-uni.edu.vn>
2026-06-18 01:48:50 +0000

    Hoan thanh file database sql va phan chia thư mục

---
+++ .github/database.sql
@@ -0,0 +1,29 @@
+CREATE TABLE SACH (
+    MaSach VARCHAR(10) PRIMARY KEY,
+    TenSach VARCHAR(100) NOT NULL,
+    TacGia VARCHAR(50),
+    NhaXuatBan VARCHAR(50),
+    NamXuatBan INT,
+    SoLuong INT DEFAULT 0
+);
+
+-- 2. Tạo bảng Độc giả
+CREATE TABLE DOC_GIA (
+    MaDocGia VARCHAR(10) PRIMARY KEY,
+    HoTen VARCHAR(50) NOT NULL,
+    NgaySinh DATE,
+    SoDienThoai VARCHAR(15),
+    Email VARCHAR(50)
+);
+
+-- 3. Tạo bảng Phiếu mượn sách
+CREATE TABLE PHIEU_MUON (
+    MaPhieu VARCHAR(10) PRIMARY KEY,
+    MaDocGia VARCHAR(10),
+    MaSach VARCHAR(10),
+    NgayMuon DATE NOT NULL,
+    NgayHenTra DATE,
+    TrangThai VARCHAR(30) DEFAULT 'Đang mượn',
+    FOREIGN KEY (MaDocGia) REFERENCES DOC_GIA(MaDocGia),
+    FOREIGN KEY (MaSach) REFERENCES SACH(MaSach)
+);
