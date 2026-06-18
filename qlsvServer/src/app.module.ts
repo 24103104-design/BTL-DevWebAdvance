f142799562d8bac20bf915711f73d4001a1d64b8
Nguyen Thanh Lam <24103104@st.phenikaa-uni.edu.vn>
2026-06-18 12:51:33 +0700

    ok

--- qlsvServer/src/app.module.ts
+++ qlsvServer/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -10,18 +11,30 @@ import { PhieuMuonModule } from './phieu-muon/phieu-muon.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'mysql-2c69a825-st-a1a9.d.aivencloud.com',
-      port: 11563,
-      username: 'avnadmin',
-      password: process.env.DB_PASSWORD,
-      database: 'defaultdb',
-      autoLoadEntities: true,
-      synchronize: true,
-      ssl: {
-        rejectUnauthorized: false,
-      },
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: Number(configService.get<string>('DB_PORT') ?? 14959),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: configService.get<string>('DB_SYNCHRONIZE') === 'true',
+        ssl:
+          configService.get<string>('DB_SSL') === 'true'
+            ? {
+                rejectUnauthorized: false,
+              }
+            : false,
+        charset: 'utf8mb4_general_ci',
+      }),
     }),
     SachModule,
     DocGiaModule,
